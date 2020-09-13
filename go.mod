c98e1a9c4abc4c0932cb37d70ff392addd0a459d
Tomokazu Hirai <tomokazu.hirai@gmail.com>
2020-09-13 11:46:31 +0900

    add main fucn

--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/viper v1.7.1
 	github.com/tenntenn/natureremo v0.0.1
+	gobot.io/x/gobot v1.14.0
 	golang.org/x/net v0.0.0-20200904194848-62affa334b73
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -9,6 +10,8 @@ import (
 	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
 	"github.com/tenntenn/natureremo"
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/platforms/joystick"
 	"golang.org/x/net/context"
 )
 
@@ -21,27 +24,70 @@ func init() {
 
 	viper.AddConfigPath(home)
 	viper.SetConfigName(".remo-joystick")
+
+	viper.AutomaticEnv()
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file: ", viper.ConfigFileUsed())
+	}
 }
 
 func main() {
 	token := viper.GetString("token")
-	aApl := viper.GetString("aApl")
-	aSig := viper.GetString("aSig")
+	aApl := viper.GetString("ButtonA.apl")
+	aSig := viper.GetString("ButtonA.sig")
+	bApl := viper.GetString("ButtonB.apl")
+	bSig := viper.GetString("ButtonB.sig")
+
+	// fmt.Println(bApl)
+	// fmt.Println(bSig)
+	// os.Exit(0)
 
 	cli := natureremo.NewClient(token)
 	ctx := context.Background()
 
-	a, err := remo.GetAppliance(ctx, cli, aApl)
-	if err != nil {
-		log.Fatal(err)
+	joystickAdaptor := joystick.NewAdaptor()
+	// stick := joystick.NewDriver(joystickAdaptor, joystick.Xbox360)
+	stick := joystick.NewDriver(joystickAdaptor, "xbox360")
+
+	work := func() {
+		stick.On(joystick.APress, func(data interface{}) {
+			if err := remoSend(cli, ctx, aApl, aSig); err != nil {
+				log.Fatal(err)
+			}
+		})
+		stick.On(joystick.BPress, func(data interface{}) {
+			if err := remoSend(cli, ctx, bApl, bSig); err != nil {
+				log.Fatal(err)
+			}
+		})
 	}
 
-	s := remo.GetSignal(a.Signals, aSig)
+	robot := gobot.NewRobot("joystickBot",
+		[]gobot.Connection{joystickAdaptor},
+		[]gobot.Device{stick},
+		work,
+	)
+	robot.Start()
+}
+
+func remoSend(cli *natureremo.Client, ctx context.Context, apl, sig string) error {
+	a, err := remo.GetAppliance(ctx, cli, apl)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	s := remo.GetSignal(a.Signals, sig)
 	if s == nil {
+		var errNotFound = errors.New("Signal Not Found")
 		log.Fatal("signal which you specified not found.")
+		return errNotFound
 	}
 
 	if err := cli.SignalService.Send(ctx, s); err != nil {
 		log.Fatal(err)
+		return err
 	}
+
+	return nil
 }
