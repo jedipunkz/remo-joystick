2fd65f5ce9e3839184b8c28346761056f0ded2c3
Tomokazu Hirai <tomokazu.hirai@gmail.com>
2020-09-13 10:05:56 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+	"github.com/tenntenn/natureremo"
+	"golang.org/x/net/context"
+)
+
+func init() {
+	home, err := homedir.Dir()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	viper.AddConfigPath(home)
+	viper.SetConfigName(".remo-controller")
+}
+
+func main() {
+	token := viper.GetString("token")
+	aApl := viper.GetString("aApl")
+	aSig := viper.GetString("aSig")
+
+	cli := natureremo.NewClient(token)
+	ctx := context.Background()
+
+	a, err := getAppliance(ctx, cli, aApl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := getSignal(a.Signal, aSig)
+	if s == nil {
+		log.Fatal("signal which you specified not found.")
+	}
+
+	if err := cli.SignalService.Send(ctx, s); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ pkg/remo/remo.go
@@ -0,0 +1,32 @@
+package remo
+
+import (
+	"context"
+	"errors"
+
+	"github.com/tenntenn/natureremo"
+)
+
+func getAppliance(ctx context.Context, cli *natureremo.Client, name string) (*natureremo.Appliance, error) {
+	as, err := cli.ApplianceService.GetAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, a := range as {
+		if a.Nickname == name {
+			return a, nil
+		}
+	}
+
+	return nil, errors.New("appliance not found")
+}
+
+func getSignal(ss []*natureremo.Signal, name string) *natureremo.Signal {
+	for _, s := range ss {
+		if s.Name == name {
+			return s
+		}
+	}
+	return nil
+}
